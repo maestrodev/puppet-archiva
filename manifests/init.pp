0f53bc8abab103390523a42c7efa16f3024794ba
Carlos Sanchez <csanchez@maestrodev.com>
2011-09-27 20:50:27 +0200

    Add ASL license

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,22 @@
-class archiva {
-	#wget it
-	include wget
+# Copyright 2011 MaestroDev
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-	user { "$archiva_user":
+class archiva {
+  #wget it
+  include wget
+
+  user { "$archiva_user":
     ensure     => present,
     home       => "$archiva_home/$archiva_user",
     managehome => false,
@@ -34,10 +48,10 @@ class archiva {
     require => File["$archiva_home/archiva"],
   } ->
   file { "$archiva_home/archiva/bin/wrapper-linux-x86-32":
-  	ensure => absent,
+    ensure => absent,
   } ->
   file { "$archiva_home/archiva/lib/libwrapper-linux-x86-32.so":
-  	ensure => absent,
+    ensure => absent,
   } ->
   file { "/etc/init.d/archiva":
     ensure  => link,

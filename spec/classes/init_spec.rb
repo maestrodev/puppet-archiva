43d62b7ef9e0d695b52dae007c9896a4ed94b638
Carlos Sanchez <csanchez@maestrodev.com>
2014-02-21 19:58:21 +0100

    Update rspec-puppet and add compilation specs

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,15 +1,12 @@
-require "#{File.join(File.dirname(__FILE__),'..','spec_helper')}"
-
-ARCHIVA_VERSION = "1.3.5"
-DEFAULT_PARAMS = {
-  :version => ARCHIVA_VERSION
-}
+require 'spec_helper'
 
 describe 'archiva' do
-  let (:params) { DEFAULT_PARAMS }
+  let(:version) { "1.3.5" }
+  let(:params) {{ :version => version }}
+  let(:jetty_config_file) { '/var/local/archiva/conf/jetty.xml' }
 
-  context "when downloading archiva from 1.3.5 onwards" do
-    let(:params) { { :version => "1.3.5" } }
+  context "when downloading archiva from 1.3.5 onwards", :compile do
+    let(:version) { "1.3.5" }
 
     it do should contain_wget__fetch('archiva_download').with(
         'source'      => "http://archive.apache.org/dist/archiva/1.3.5/binaries/apache-archiva-1.3.5-bin.tar.gz",
@@ -19,8 +16,8 @@ describe 'archiva' do
     end
   end
 
-  context "when downloading archiva from new version" do
-    let(:params) { { :version => "2.0.0" } }
+  context "when downloading archiva from new version", :compile do
+    let(:version) { "2.0.0" }
 
     it do should contain_wget__fetch('archiva_download').with(
         'source'      => "http://archive.apache.org/dist/archiva/2.0.0/binaries/apache-archiva-2.0.0-bin.tar.gz",
@@ -30,21 +27,21 @@ describe 'archiva' do
     end
   end
 
-  context "when downloading archiva from a mirror" do
-    let(:params) { {
+  context "when downloading archiva from a mirror", :compile do
+    let(:params) { super().merge({
       :apache_mirror => "http://mirror.internode.on.net/pub/apache"
-    }.merge DEFAULT_PARAMS }
+    }) }
 
     it do should contain_wget__fetch('archiva_download').with(
-        'source'      => "http://mirror.internode.on.net/pub/apache/archiva/#{ARCHIVA_VERSION}/binaries/apache-archiva-#{ARCHIVA_VERSION}-bin.tar.gz",
+        'source'      => "http://mirror.internode.on.net/pub/apache/archiva/#{version}/binaries/apache-archiva-#{version}-bin.tar.gz",
         'user'        => nil,
         'password'    => nil
     ) 
     end
   end
 
-  context "when downloading archiva from an old version" do
-    let(:params) { { :version => "1.3.4" } }
+  context "when downloading archiva from an old version", :compile do
+    let(:version) { "1.3.4" }
 
     it do should contain_wget__fetch('archiva_download').with(
         'source'      => "http://archive.apache.org/dist/archiva/binaries/apache-archiva-1.3.4-bin.tar.gz",
@@ -54,8 +51,8 @@ describe 'archiva' do
     end
   end
 
-  context "when downloading archiva from 1.4-M1 version" do
-    let(:params) { { :version => "1.4-M1" } }
+  context "when downloading archiva from 1.4-M1 version", :compile do
+    let(:version) { "1.4-M1" }
 
     it do should contain_wget__fetch('archiva_download').with(
         'source'      => "http://archive.apache.org/dist/archiva/binaries/apache-archiva-1.4-M1-bin.tar.gz",
@@ -65,111 +62,110 @@ describe 'archiva' do
     end
   end
 
-  context "when downloading archiva from another repo" do
-    let(:params) { { :repo => {
+  context "when downloading archiva from another repo", :compile do
+    let(:params) { super().merge({ :repo => {
         'url'      => 'http://repo1.maven.org/maven2',
       }
-    }.merge DEFAULT_PARAMS }
+    }) }
 
     it 'should fetch archiva from repo' do
       should contain_wget__fetch('archiva_download').with(
-        'source'      => "http://repo1.maven.org/maven2/org/apache/archiva/archiva-jetty/#{ARCHIVA_VERSION}/archiva-jetty-#{ARCHIVA_VERSION}-bin.tar.gz",
+        'source'      => "http://repo1.maven.org/maven2/org/apache/archiva/archiva-jetty/#{version}/archiva-jetty-#{version}-bin.tar.gz",
         'user'        => nil,
         'password'    => nil)
     end
   end
 
-  context "when downloading archiva from another repo with credentials" do
-    let(:params) { { :repo => {
+  context "when downloading archiva from another repo with credentials", :compile do
+    let(:params) { super().merge({ :repo => {
         'url'      => 'http://repo1.maven.org/maven2',
         'username' => 'u',
         'password' => 'p'
       }
-    }.merge DEFAULT_PARAMS }
+    }) }
 
     it 'should fetch archiva with username and password' do
       should contain_wget__authfetch('archiva_download').with(
-        'source'      => "http://repo1.maven.org/maven2/org/apache/archiva/archiva-jetty/#{ARCHIVA_VERSION}/archiva-jetty-#{ARCHIVA_VERSION}-bin.tar.gz",
+        'source'      => "http://repo1.maven.org/maven2/org/apache/archiva/archiva-jetty/#{version}/archiva-jetty-#{version}-bin.tar.gz",
         'user'        => 'u',
         'password'    => 'p')
     end
   end
 
-  jetty_config_file = '/var/local/archiva/conf/jetty.xml'
-  context "with default version" do
+  context "with default version", :compile do
     it "should configure the archiva JDBC connection" do
-      content = catalogue.resource('file', jetty_config_file).send(:parameters)[:content]
+      content = subject.resource('file', jetty_config_file).send(:parameters)[:content]
       content.should =~ %r[jdbc/archiva]
       content.should_not =~ %r[org\.eclipse]
     end
   end
 
-  context "with 1.4-M1" do
-    let(:params) { { :version => "1.4-M1" } }
+  context "with 1.4-M1", :compile do
+    let(:version) { "1.4-M1" }
     it "should configure the archiva JDBC connection" do
-      content = catalogue.resource('file', jetty_config_file).send(:parameters)[:content]
+      content = subject.resource('file', jetty_config_file).send(:parameters)[:content]
       content.should_not =~ %r[jdbc/archiva]
       content.should_not =~ %r[org\.eclipse]
     end
   end
 
-  context "with recent version" do
-    let(:params) { { :version => "1.4-M3" } }
+  context "with recent version", :compile do
+    let(:version) { "1.4-M3" }
     it "should not configure the archiva JDBC connection" do
       should contain_file(jetty_config_file)
-      content = catalogue.resource('file', jetty_config_file).send(:parameters)[:content]
+      content = subject.resource('file', jetty_config_file).send(:parameters)[:content]
       content.should_not =~ %r[jdbc/archiva]
       content.should =~ %r[org\.eclipse]
     end
   end
 
   security_config_file = '/var/local/archiva/conf/security.properties'
-  context 'when application URL is not set' do    
+  context 'when application URL is not set', :compile do    
     it 'should set the HOME variable correctly in the startup script' do
-      content = catalogue.resource('file', security_config_file).send(:parameters)[:content]
+      content = subject.resource('file', security_config_file).send(:parameters)[:content]
       content.should =~ %r[application\.url = http://localhost:8080/archiva/]
     end
   end
   
 
-  context 'when application URL is set' do
-    let(:params) { { :application_url => 'http://someurl/' }.merge DEFAULT_PARAMS }
+  context 'when application URL is set', :compile do
+    let(:params) { super().merge({ :application_url => 'http://someurl/' }) }
     
     it 'should set the HOME variable correctly in the startup script' do
-      content = catalogue.resource('file', security_config_file).send(:parameters)[:content]
+      content = subject.resource('file', security_config_file).send(:parameters)[:content]
       content.should =~ %r[application\.url = http://someurl/]
     end
   end
 
-  context "when cookie path is set" do
-    let(:params) { { :cookie_path => "/" }.merge DEFAULT_PARAMS }
+  context "when cookie path is set", :compile do
+    let(:params) { super().merge({ :cookie_path => "/" }) }
 
     security_config_file="/var/local/archiva/conf/security.properties"
     it "should set the cookie paths" do
       should contain_file(security_config_file)
-      content = catalogue.resource('file', security_config_file).send(:parameters)[:content]
+      content = subject.resource('file', security_config_file).send(:parameters)[:content]
       content.should =~ %r[security\.signon\.path=/]
       content.should =~ %r[security\.rememberme\.path=/]
     end
   end
 
-  context "when cookie path is not set" do
+  context "when cookie path is not set", :compile do
     security_config_file="/var/local/archiva/conf/security.properties"
     it "should not set the cookie paths" do
       should contain_file(security_config_file)
-      content = catalogue.resource('file', security_config_file).send(:parameters)[:content]
+      content = subject.resource('file', security_config_file).send(:parameters)[:content]
       content.should_not =~ %r[security\.signon\.path]
       content.should_not =~ %r[security\.rememberme\.path]
     end
   end
 
-  context "when upload size is set" do
-    let(:params) { { :max_upload_size => 10485760 }.merge DEFAULT_PARAMS }
+  context "when upload size is set", :compile do
+    let(:params) { super().merge({ :max_upload_size => 10485760 }) }
 
     it { should contain_augeas('set-upload-size') }
   end
 
-  context "when upload size is not set" do
+  context "when upload size is not set", :compile do
     it { should_not contain_augeas('set-upload-size') }
   end
 end
